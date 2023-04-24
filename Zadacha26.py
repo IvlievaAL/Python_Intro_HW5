60371d53788f3dd6ea2dc375f48bd211d68dd8d3
AlexIv <ivlieva.medphyslab.com>
2023-04-24 20:56:52 +0300

    Z26 Code has been completed and works

--- Zadacha26.py
+++ Zadacha26.py
@@ -4,3 +4,15 @@
 
 # A = 3; B = 5 -> 243 (3⁵)
 # A = 2; B = 3 -> 8
+
+def a_in_degree_b(a, b, nashe_a):
+    if b == 1:
+        return a
+    a = a*nashe_a
+    b -= 1
+    return a_in_degree_b(a, b, nashe_a)
+
+a = int(input("Введите число a: "))
+nashe_a = a
+b = int(input("Введите число b: "))
+print(a_in_degree_b(a, b, nashe_a))
